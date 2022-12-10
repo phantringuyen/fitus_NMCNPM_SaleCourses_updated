2eaa05d28dc56905741dbf57d699b4c10f5c3207
Phan Trí Nguyên <74893651+phantringuyen@users.noreply.github.com>
2022-12-10 11:49:32 +0700

    Update Appbar.js
    renovating the function with different sizes, position and style of button in appbar

--- frontend/salecourses/src/components/Appbar/Appbar.js
+++ frontend/salecourses/src/components/Appbar/Appbar.js
@@ -32,7 +32,7 @@ const Search = styled('div')(({ theme }) => ({
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
+  padding: theme.spacing(1, 2),
   height: '100%',
   position: 'absolute',
   pointerEvents: 'none',
